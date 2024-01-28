01e03a84d87801b3e3a1a5467c1462295090cf0b
alisabry123 <alisabry321@gmail.com>
2024-01-28 22:25:21 +0200

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2 = 0;
 	unsigned int len1 = 0;
 	unsigned int i = 0;
-	char *new;
+	char *new = NULL;
 
 	while (s1[len1] != 0)
 		len1++;
@@ -24,6 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	new = malloc(len1 + len2 + 1);
+	if (new == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		new[i] = s1[i];
@@ -31,5 +33,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len2 && i < n; i++)
 		new[i + len1] = s2[i];
 
+	new[i + len1] = 0;
 	return (new);
 }
