5c79e75a2de27a0ab3fc52b75d0174b328f1a5cd
alisabry123 <alisabry321@gmail.com>
2024-01-28 22:36:22 +0200

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	char *new = NULL;
 
-	while (s1[len1] != 0)
-		len1++;
+	if (s1 == NULL)
+		len1 = 0;
+	else
+	{
+		while (s1[len1] != 0)
+			len1++;
+	}
 
-	while (s2[len2] != 0)
-		len2++;
+	if (s2 == NULL)
+		len2 = 0;
+	else
+	{
+		while (s2[len2] != 0)
+			len2++;
+	}
 
 	new = malloc(len1 + len2 + 1);
 	if (new == NULL)
